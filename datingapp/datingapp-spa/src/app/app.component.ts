8e1b1a8dd09555cf6708d60648d6dcf334b4f80c
Agaba Kabunga <agaba_k@hotmail.com>
2020-12-30 17:35:14 -0600

    Add async  user template

--- datingapp/datingapp-spa/src/app/app.component.ts
+++ datingapp/datingapp-spa/src/app/app.component.ts
@@ -1,21 +1,42 @@
+
 import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  implements OnInit  {
+export class AppComponent implements OnInit {
   title = 'Dating app';
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit() {
-    this.http.get('https://localhost:5001/api/Users').subscribe((response) => {
-        this.users =  response;
-    }, error => {console.log(error); });
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/Users').subscribe(
+      (response) => {
+        this.users = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
